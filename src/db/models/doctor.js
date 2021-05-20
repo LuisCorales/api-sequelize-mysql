a96312fd0c4effbce624ffc03b7f1df5530c8656
Luis Corales <luisaugcorales@gmail.com>
2021-05-20 18:57:58 -0500

    Update doctor.js

--- src/db/models/doctor.js
+++ src/db/models/doctor.js
@@ -3,34 +3,46 @@ const Sequelize = require('sequelize');
 
 /** Define doctor model */ 
 const Doctor = db.define('doctor', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
     firstName: {
-        type: Sequelize.STRING(60),
-        allowNull: false
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\'\s]*)$/g,
+                msg: "surname can only contain letters and '."
+            },
+            len: {
+                args: [3, 60],
+                msg: "firstName can only have from 3 to 60 letters."
+            },
+            notNull: {
+                msg: "firstName cannot be null."
+            }
+        }
     },
     surname: {
-        type: Sequelize.STRING(60),
-        allowNull: false
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\'\s]*)$/g,
+                msg: "surname can only contain letters and '."
+            },
+            len: {
+                args: [3, 60],
+                msg: "surname can only have from 3 to 60 letters."
+            },
+            notNull: {
+                msg: "surname cannot be null."
+            }
+        }
     },
     speciality: {
-        type: Sequelize.STRING(20),
+        type: Sequelize.STRING(100),
         allowNull: false
     },
-    hospitalId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'hospitals',
-            key: 'id'
-        },
-        onUpdate: 'NO ACTION',
-        onDelete: 'NO ACTION'
-    },
+}, {
+    timestamps: false
 });
 
 module.exports = Doctor;

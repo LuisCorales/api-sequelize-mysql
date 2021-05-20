143aa8cd4ccc6fe6ee2cacd441056ff553d7b0f8
Luis Corales <luisaugcorales@gmail.com>
2021-05-20 12:38:52 -0500

    Update appointment.js

--- src/db/models/appointment.js
+++ src/db/models/appointment.js
@@ -2,32 +2,12 @@ const db = require('../database');
 const Sequelize = require('sequelize');
 
 // Define appointment model
-module.exports = db.define('appointment', {
+const Appointment = db.define('appointment', {
     id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true
-    },
-    patientId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'patients',
-            key: 'id',
-        },
-        onUpdate: 'NO ACTION',
-        onDelete: 'NO ACTION'
-    },
-    doctorId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'doctors',
-            key: 'id'
-        },
-        onUpdate: 'NO ACTION',
-        onDelete: 'NO ACTION'
     },
     startTime: {
         type: Sequelize.DATE,

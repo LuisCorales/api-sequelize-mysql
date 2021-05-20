e2bc807ae61eec0692372399fe0c8956ba996290
Luis Corales <luisaugcorales@gmail.com>
2021-05-20 18:57:53 -0500

    Update appointment.js

--- src/db/models/appointment.js
+++ src/db/models/appointment.js
@@ -3,20 +3,36 @@ const Sequelize = require('sequelize');
 
 /** Define appointment model */ 
 const Appointment = db.define('appointment', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
     startTime: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            isDate: {
+                args: true,
+                msg: "startTime can only be a date."
+            },
+            isAfter: {
+                args: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString(),
+                msg: "startTime cannot be start before today."
+            }
+        }
     },
     endTime: { 
         type: Sequelize.DATE,
         allowNull: false,
-    }
+        validate: {
+            isDate: {
+                args: true,
+                msg: "endTime can only be a date."
+            }, 
+            isAfter: {
+                args: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString(),
+                msg: "endTime cannot end before today."
+            }
+        }
+    },
+}, {
+    timestamps: false
 });
 
 module.exports = Appointment;

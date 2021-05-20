c464a3a6aff3591a7a9ee847c172067515d5fa6f
Luis Corales <luisaugcorales@gmail.com>
2021-05-20 18:58:09 -0500

    Update patient.js

--- src/db/models/patient.js
+++ src/db/models/patient.js
@@ -3,26 +3,50 @@ const Sequelize = require('sequelize');
 
 /** Define patient model */ 
 const Patient = db.define('patient', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
     firstName: {
-        type: Sequelize.STRING(60),
-        allowNull: false
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\'\s]*)$/g,
+                msg: "surname can only contain letters and '."
+            },
+            len: {
+                args: [3, 60],
+                msg: "firstName can only have from 3 to 60 letters."
+            },
+            notNull: {
+                msg: "firstName cannot be null."
+            }
+        }
     },
     surname: {
-        type: Sequelize.STRING(60),
-        allowNull: false
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\'\s]*)$/g,
+                msg: "surname can only contain letters and '."
+            },
+            len: {
+                args: [3, 60],
+                msg: "surname can only have from 3 to 60 letters."
+            },
+            notNull: {
+                msg: "surname cannot be null."
+            }
+        }
     },
     idDocument: {
         type: Sequelize.STRING(20),
         allowNull: false,
-        unique: true
+        unique: true,
     },
-    pathology: Sequelize.STRING(100)
+    pathology: {
+        type: Sequelize.STRING(100),
+    },
+}, {
+    timestamps: false
 });
 
 module.exports = Patient;

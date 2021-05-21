1e1af7e63626f6fbc099ed62bff4094f129ba605
Luis Corales <luisaugcorales@gmail.com>
2021-05-21 17:21:35 -0500

    Update doctor.js

--- src/models/doctor.js
+++ src/models/doctor.js
@@ -9,7 +9,7 @@ const Doctor = db.define('doctor', {
         validate: {
             is: {
                 args: /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\'\s]*)$/g,
-                msg: "surname can only contain letters and '."
+                msg: "firstName can only contain letters and '."
             },
             len: {
                 args: [3, 60],

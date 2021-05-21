0721da97a35feaa621f9c4e4081596fd0f7b6e13
Luis Corales <luisaugcorales@gmail.com>
2021-05-21 17:20:31 -0500

    Update 20210521002356-create_appointment_table.js

--- src/db/migrations/20210521002356-create_appointment_table.js
+++ src/db/migrations/20210521002356-create_appointment_table.js
@@ -2,20 +2,41 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('appointments', {
+      startTime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        validate: {
+          isDate: {
+            args: true,
+            msg: "startTime can only be a date."
+          },
+          isAfter: {
+            args: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString(),
+            msg: "startTime cannot be start before today."
+          }
+        }
+      },
+      endTime: { 
+        type: Sequelize.DATE,
+        allowNull: false,
+        validate: {
+          isDate: {
+            args: true,
+            msg: "endTime can only be a date."
+          }, 
+          isAfter: {
+            args: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString(),
+            msg: "endTime cannot end before today."
+          }
+        }
+      },
+    }, {
+      timestamps: false
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('appointments');
   }
 };
